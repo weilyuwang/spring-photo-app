d76bcca6f52e95ae33f8879e862c90400025f285
weilyuwang <weilyuwang@gmail.com>
2020-03-04 21:23:54 -0500

    config zuul gateway microservice

--- photoapp-zuul-gateway/src/main/java/com/weilyu/photoapp/photoappgateway/PhotoappZuulGatewayApplication.java
+++ photoapp-zuul-gateway/src/main/java/com/weilyu/photoapp/photoappgateway/PhotoappZuulGatewayApplication.java
@@ -2,8 +2,12 @@ package com.weilyu.photoapp.photoappgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class PhotoappZuulGatewayApplication {
 
     public static void main(String[] args) {
