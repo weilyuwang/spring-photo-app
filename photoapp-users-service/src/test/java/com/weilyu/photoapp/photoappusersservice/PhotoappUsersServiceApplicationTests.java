00891411f4aa9e90d8549551e0952dc010e66c9e
weilyuwang <weilyuwang@gmail.com>
2020-03-04 19:35:44 -0500

    create users microservice

---
+++ photoapp-users-service/src/test/java/com/weilyu/photoapp/photoappusersservice/PhotoappUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.photoappusersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappUsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
