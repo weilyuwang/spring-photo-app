88c17ab419e9fa7aad8d3f8e1716de8ddbc11641
weilyuwang <weilyuwang@gmail.com>
2020-03-04 21:00:26 -0500

    create account management microservice starter project

---
+++ photoapp-account-management/src/main/java/com/weilyu/photoapp/accountmanagement/PhotoappAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.accountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappAccountManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappAccountManagementApplication.class, args);
+    }
+
+}
---
+++ photoapp-account-management/src/test/java/com/weilyu/photoapp/accountmanagement/PhotoappAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.accountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappAccountManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
