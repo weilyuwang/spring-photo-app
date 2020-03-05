949c69255da100e57908017763d97aa8ff717894
weilyuwang <weilyuwang@gmail.com>
2020-03-04 19:35:13 -0500

    create discovery server

---
+++ photoapp-discovery-server/src/main/java/com/weilyu/photoapp/photoappdiscoveryserver/PhotoappDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.weilyu.photoapp.photoappdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PhotoappDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ photoapp-discovery-server/src/test/java/com/weilyu/photoapp/photoappdiscoveryserver/PhotoappDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.photoappdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
