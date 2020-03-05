d28dbd11dbd6b674b3e4e91dfcf9a4943e42209d
weilyuwang <weilyuwang@gmail.com>
2020-03-04 22:43:53 -0500

    config users service's user controller to display port number of the current running instance - to illustrate that zuul gateway is working as a load balancer.

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/ui/controllers/UsersController.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/ui/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.weilyu.photoapp.photoappusersservice.ui.controllers;
 
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    private final Environment env;
+
+    public UsersController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+
+        return "Working on port " + env.getProperty("local.server.port");
     }
 
 }
