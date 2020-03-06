f195fa6b3edeb0891ddc1e17de6b0336db56b12a
weilyuwang <weilyuwang@gmail.com>
2020-03-05 22:14:25 -0500

    implements users service

---
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/service/UsersService.java
@@ -0,0 +1,9 @@
+package com.weilyu.photoapp.photoappusersservice.service;
+
+import com.weilyu.photoapp.photoappusersservice.shared.UserDto;
+
+public interface UsersService {
+
+    UserDto createUser(UserDto userDetails);
+
+}
---
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/service/UsersServiceImpl.java
@@ -0,0 +1,14 @@
+package com.weilyu.photoapp.photoappusersservice.service;
+
+
+import com.weilyu.photoapp.photoappusersservice.shared.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl implements UsersService{
+
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+        return null;
+    }
+}
