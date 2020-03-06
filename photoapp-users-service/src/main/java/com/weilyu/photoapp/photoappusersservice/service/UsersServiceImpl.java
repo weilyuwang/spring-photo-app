bc66c068304c6c2a1e35b9b2ba1b4b272b86aa2e
weilyuwang <weilyuwang@gmail.com>
2020-03-05 22:55:59 -0500

    user ModelMapper to map DTO into JPA Entity

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/service/UsersServiceImpl.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/service/UsersServiceImpl.java
@@ -1,7 +1,11 @@
 package com.weilyu.photoapp.photoappusersservice.service;
 
 
+import com.weilyu.photoapp.photoappusersservice.data.UserEntity;
+import com.weilyu.photoapp.photoappusersservice.data.UserRepository;
 import com.weilyu.photoapp.photoappusersservice.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -9,11 +13,30 @@ import java.util.UUID;
 @Service
 public class UsersServiceImpl implements UsersService{
 
+    private final UserRepository userRepository;
+
+    public UsersServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDto createUser(UserDto userDetails) {
 
         // set an unique user ID (UUID)
         userDetails.setUserId(UUID.randomUUID().toString());
+
+        // user ModelMapper to map DTO into JPA Entity ( field variable names in DTO and Entity must match (be the same) )
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+
+        // encrypt user password before persistence into database
+        // for now, hard code it. TODO encrypt user password
+        userEntity.setEncryptedPassword("test_encrypt_pass");
+
+        // save entity into database using repository
+        userRepository.save(userEntity);
 
         return null;
     }
