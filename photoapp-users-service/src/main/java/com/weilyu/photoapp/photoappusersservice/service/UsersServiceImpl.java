8b96f4ebcb8a604c2b423745a34c2ef61cd01e92
weilyuwang <weilyuwang@gmail.com>
2020-03-05 22:27:24 -0500

    generate user id from UserDTO (UUID)

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/service/UsersServiceImpl.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/service/UsersServiceImpl.java
@@ -4,11 +4,17 @@ package com.weilyu.photoapp.photoappusersservice.service;
 import com.weilyu.photoapp.photoappusersservice.shared.UserDto;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UsersServiceImpl implements UsersService{
 
     @Override
     public UserDto createUser(UserDto userDetails) {
+
+        // set an unique user ID (UUID)
+        userDetails.setUserId(UUID.randomUUID().toString());
+
         return null;
     }
 }
