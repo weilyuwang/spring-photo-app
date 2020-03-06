c1c2a8f54d07a8a97f0b225468a32c9c369682f6
weilyuwang <weilyuwang@gmail.com>
2020-03-05 21:19:56 -0500

    enable h2 in users service

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/ui/controllers/UsersController.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/ui/controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.weilyu.photoapp.photoappusersservice.ui.controllers;
 
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,12 @@ public class UsersController {
 
     @GetMapping("/status/check")
     public String status() {
-
         return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "createUser method is called.";
     }
 
 }
