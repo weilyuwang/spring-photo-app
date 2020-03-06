9824e0a8a43cad95feeeeefc24ee303616015e42
weilyuwang <weilyuwang@gmail.com>
2020-03-05 23:10:26 -0500

    update createUser controller method to handle create user post request

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/ui/controllers/UsersController.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/ui/controllers/UsersController.java
@@ -1,7 +1,11 @@
 package com.weilyu.photoapp.photoappusersservice.ui.controllers;
 
 
+import com.weilyu.photoapp.photoappusersservice.service.UsersService;
+import com.weilyu.photoapp.photoappusersservice.shared.UserDto;
 import com.weilyu.photoapp.photoappusersservice.ui.model.CreateUserRequestModel;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,8 +17,11 @@ public class UsersController {
 
     private final Environment env;
 
-    public UsersController(Environment env) {
+    private final UsersService usersService;
+
+    public UsersController(Environment env, UsersService usersService) {
         this.env = env;
+        this.usersService = usersService;
     }
 
     @GetMapping("/status/check")
@@ -24,6 +31,15 @@ public class UsersController {
 
     @PostMapping
     public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {   // convert JSON payload into CreateUserRequestModel object
+
+        // user ModelMapper to map POST request body model into DTO object
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+
+        usersService.createUser(userDto);
+
         return "createUser method is called.";
     }
 
