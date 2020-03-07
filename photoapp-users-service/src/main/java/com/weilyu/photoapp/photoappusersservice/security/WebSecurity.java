f8652f0b64c64d43dd83960ee7a613b243ceaf04
weilyuwang <weilyuwang@gmail.com>
2020-03-06 22:07:52 -0500

    config users service to only accept requests coming from Zuul gateway's IP

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/security/WebSecurity.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.weilyu.photoapp.photoappusersservice.security;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,9 +11,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private final Environment environment;
+
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
+        http.headers().frameOptions().disable();
     }
 }
