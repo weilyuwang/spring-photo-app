eaa615f684502fa25e914ed90763fb10e270bf34
weilyuwang <weilyuwang@gmail.com>
2020-03-08 21:06:49 -0400

    secure http requests coming to zuul gateway

--- photoapp-zuul-gateway/src/main/java/com/weilyu/photoapp/photoappgateway/security/WebSecurity.java
+++ photoapp-zuul-gateway/src/main/java/com/weilyu/photoapp/photoappgateway/security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.weilyu.photoapp.photoappgateway.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +22,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                .antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
+                .anyRequest().authenticated();  // any other requests should be authenticated
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
