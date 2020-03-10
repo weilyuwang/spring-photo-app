1ec50bb80736681030bb3a6e0cf4408ad10bdc47
weilyuwang <weilyuwang@gmail.com>
2020-03-09 20:38:59 -0400

    create spring cloud config server

---
+++ photoapp-config-server/src/test/java/com/weilyu/photoapp/photoappconfigserver/PhotoappConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.photoappconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
