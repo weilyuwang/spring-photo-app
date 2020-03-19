a4559e89c867465546cedfc9517e28a3aa299fd5
weilyuwang <weilyuwang@gmail.com>
2020-03-19 16:59:39 -0400

    enable Feign Logging for HTTP requests

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/PhotoappUsersServiceApplication.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/PhotoappUsersServiceApplication.java
@@ -1,5 +1,6 @@
 package com.weilyu.photoapp.photoappusersservice;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -27,5 +28,11 @@ public class PhotoappUsersServiceApplication {
     @LoadBalanced //enable client-side load balancing
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    //Feign Logger Level Bean
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
