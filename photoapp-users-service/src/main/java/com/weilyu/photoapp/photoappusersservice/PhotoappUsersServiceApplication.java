211ca3dddb71d983d197dcaa6fcc7d165b29e1b0
weilyuwang <weilyuwang@gmail.com>
2020-03-19 18:04:06 -0400

    add Hystrix Circuit Breaker into Users Microservice

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/PhotoappUsersServiceApplication.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/PhotoappUsersServiceApplication.java
@@ -4,6 +4,7 @@ import com.weilyu.photoapp.photoappusersservice.shared.FeignErrorDecoder;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoappUsersServiceApplication {
 
     public static void main(String[] args) {
