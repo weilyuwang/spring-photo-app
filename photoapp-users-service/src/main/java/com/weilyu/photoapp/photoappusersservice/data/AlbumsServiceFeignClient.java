2e14131c6d6f83e76a0401bb14978d5715459700
weilyuwang <weilyuwang@gmail.com>
2020-03-19 18:21:33 -0400

    create fallback class and method for circuit breaker

--- photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/data/AlbumsServiceFeignClient.java
+++ photoapp-users-service/src/main/java/com/weilyu/photoapp/photoappusersservice/data/AlbumsServiceFeignClient.java
@@ -3,14 +3,27 @@ package com.weilyu.photoapp.photoappusersservice.data;
 
 import com.weilyu.photoapp.photoappusersservice.ui.model.AlbumResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "albums-service")
+@FeignClient(name = "albums-service", fallback = AlbumsFallback.class)
 public interface AlbumsServiceFeignClient {
 
     @GetMapping("/users/{id}/albums")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+
+@Component
+class AlbumsFallback implements AlbumsServiceFeignClient {
+
+    // callback method signature must match with the original method
+    @Override
+    public List<AlbumResponseModel> getAlbums(String id) {
+        return new ArrayList<>();
+    }
 }
