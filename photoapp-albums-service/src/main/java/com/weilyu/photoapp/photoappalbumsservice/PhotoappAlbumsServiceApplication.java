4c05077ff270354115d4b6a760fb4797eaf509aa
weilyuwang <weilyuwang@gmail.com>
2020-03-18 21:22:05 -0400

    create albums microservice starter module

---
+++ photoapp-albums-service/src/main/java/com/weilyu/photoapp/photoappalbumsservice/PhotoappAlbumsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.photoappalbumsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappAlbumsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappAlbumsServiceApplication.class, args);
+    }
+
+}
---
+++ photoapp-albums-service/src/test/java/com/weilyu/photoapp/photoappalbumsservice/PhotoappAlbumsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.photoappalbumsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappAlbumsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
