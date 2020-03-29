93ab27ee478d6fb4b170ac4318e8d9fdeb002e2b
weilyuwang <weilyuwang@gmail.com>
2020-03-28 21:11:43 -0400

    try to expose actuator/httptrace endpoint

--- photoapp-zuul-gateway/src/main/java/com/weilyu/photoapp/photoappgateway/PhotoappZuulGatewayApplication.java
+++ photoapp-zuul-gateway/src/main/java/com/weilyu/photoapp/photoappgateway/PhotoappZuulGatewayApplication.java
@@ -1,9 +1,12 @@
 package com.weilyu.photoapp.photoappgateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +15,11 @@ public class PhotoappZuulGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhotoappZuulGatewayApplication.class, args);
+    }
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
