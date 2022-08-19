0558f744567ae8fb999ea9c09d70e103ef13d4a0
weilyuwang <weilyuwang@gmail.com>
2022-08-19 17:44:52 -0400

    add spring cloud api gateway

---
+++ photoapp-api-gateway/src/main/java/com/weilyu/photoapp/photoappapigateway/PhotoappApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.photoappapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappApiGatewayApplication.class, args);
+    }
+
+}
---
+++ photoapp-api-gateway/src/test/java/com/weilyu/photoapp/photoappapigateway/PhotoappApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilyu.photoapp.photoappapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
