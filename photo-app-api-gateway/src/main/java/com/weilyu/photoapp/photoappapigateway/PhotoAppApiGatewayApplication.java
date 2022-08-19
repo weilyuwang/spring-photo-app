c32ff7bc8cb6205c4d1fc04a774b30cae7f9cd89
weilyuwang <weilyuwang@gmail.com>
2022-08-19 18:19:43 -0400

    rename

--- photo-app-api-gateway/src/main/java/com/weilyu/photoapp/photoappapigateway/PhotoAppApiGatewayApplication.java
+++ photo-app-api-gateway/src/main/java/com/weilyu/photoapp/photoappapigateway/PhotoAppApiGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhotoappApiGatewayApplication {
+public class PhotoAppApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PhotoappApiGatewayApplication.class, args);
+        SpringApplication.run(PhotoAppApiGatewayApplication.class, args);
     }
 
 }
